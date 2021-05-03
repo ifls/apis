170c31a54cfc8092b1d7095fd795b360e850af35
ifls <ifls.heyifeng@gmail.com>
2021-05-03 16:50:04 +0800

    1. 删除go.mod

--- go.mod
+++
@@ -1,8 +0,0 @@
-module apis
-
-go 1.16
-
-require (
-	github.com/golang/protobuf v1.5.2
-	google.golang.org/genproto v0.0.0-20210429181445-86c259c2b4ab
-)
--- pb/gen.go
+++
@@ -1,4 +0,0 @@
-//go:generate protoc --proto_path=. --go_out ../go/ --go_opt paths=source_relative ./book/book.proto
-//go:generate protoc --proto_path=.  --go-grpc_out ../go/ --go-grpc_opt paths=source_relative ./book/book.proto
-//go:generate protoc --proto_path=.  --grpc-gateway_out ../go/ --grpc-gateway_opt paths=source_relative --grpc-gateway_opt logtostderr=true  ./book/book.proto
-package pb
