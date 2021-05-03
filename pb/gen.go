2036e6c236a519eb0652e16c20ff00fa1a0d9e1c
ifls <ifls.heyifeng@gmail.com>
2021-05-03 16:38:06 +0800

    1. 拷贝google/api/ano

--- pb/gen.go
+++ pb/gen.go
@@ -6,5 +6,5 @@
 
 //go:generate protoc --go_out=./user -I=./user user.proto
 
-//go:generate protoc -I=.;/Volumes/c/gitRepo/github/googleapis --go_out ./go/ --go_opt paths=source_relative --go-grpc_out ./go/ --go-grpc_opt paths=./book  ./book/book.proto
+//go:generate protoc --proto_path=. --go_out ../go/  --go-grpc_out ../go/ --grpc-gateway_opt paths=source_relative ./book/book.proto
 package pb
