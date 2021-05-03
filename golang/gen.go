2c9d43e66922251db3c5ba71404ba823a01baf65
ifls <ifls.heyifeng@gmail.com>
2021-05-03 18:34:34 +0800

    1. 重新生成book service

--- golang/gen.go
+++ golang/gen.go
@@ -1,6 +1,4 @@
-//go:generate protoc --proto_path=../pb --go_out . --go_opt paths=source_relative book/book.proto
-//go:generate protoc --proto_path=../pb  --go-grpc_out . --go-grpc_opt paths=source_relative book/book.proto
-//go:generate protoc --proto_path=../pb  --grpc-gateway_out . --grpc-gateway_opt paths=source_relative --grpc-gateway_opt logtostderr=true  book/book.proto
+//go:generate go run gen.go
 package main
 
 import (
