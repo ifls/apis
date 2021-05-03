c0d3123293b2de3bf8f184b51d42b25d8190e8bb
ifls <ifls.heyifeng@gmail.com>
2021-05-03 16:46:41 +0800

    1. 支持生成grpc和grpc-gateway代码

--- pb/gen.go
+++ pb/gen.go
@@ -1,10 +1,4 @@
-//go:generate protoc --go_out=./auth -I=./auth auth.proto
-
-//go:generate protoc --go_out=./book -I=./book book.proto
-
-//go:generate protoc --go_out=./login -I=./login login.proto
-
-//go:generate protoc --go_out=./user -I=./user user.proto
-
-//go:generate protoc --proto_path=. --go_out ../go/  --go-grpc_out ../go/ --grpc-gateway_opt paths=source_relative ./book/book.proto
+//go:generate protoc --proto_path=. --go_out ../go/ --go_opt paths=source_relative ./book/book.proto
+//go:generate protoc --proto_path=.  --go-grpc_out ../go/ --go-grpc_opt paths=source_relative ./book/book.proto
+//go:generate protoc --proto_path=.  --grpc-gateway_out ../go/ --grpc-gateway_opt paths=source_relative --grpc-gateway_opt logtostderr=true  ./book/book.proto
 package pb
