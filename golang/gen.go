7fd48c699491649f270538f75bf367a6dc22cfdc
ifls <ifls.heyifeng@gmail.com>
2021-05-03 16:54:25 +0800

    1. 重命名目录

--- golang/gen.go
+++ golang/gen.go
@@ -1,4 +1,8 @@
 //go:generate protoc --proto_path=../pb --go_out . --go_opt paths=source_relative book/book.proto
 //go:generate protoc --proto_path=../pb  --go-grpc_out . --go-grpc_opt paths=source_relative book/book.proto
 //go:generate protoc --proto_path=../pb  --grpc-gateway_out . --grpc-gateway_opt paths=source_relative --grpc-gateway_opt logtostderr=true  book/book.proto
-package _go
+package main
+
+func main() {
+
+}
